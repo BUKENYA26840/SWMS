d7614d4dfb24613339969c017dfa9a24c8c05b8f
26840 <bukenyamustafa15@gmail.com>
2026-05-15 11:57:20 +0200

    Change database provider to Npgsql
    Updated database context configuration to use Npgsql instead of SQL Server.

--- Program.cs
+++ Program.cs
@@ -13,8 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-        sqlOptions => sqlOptions.EnableRetryOnFailure()));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IWalletService, WalletService>();
